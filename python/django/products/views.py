c85238d070a7a682785e83071d65c4c4f4b4b8c5
Gerald Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-08-23 23:55:50 +0000

    migration

--- python/django/products/views.py
+++ python/django/products/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
     return HttpResponse("Hello World")
+
+
+def new(request):
+    return HttpResponse("New Product")
+
+    
