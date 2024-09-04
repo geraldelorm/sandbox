70ef4d0e7930f4c719aedaf998c00e13cb119e06
geraldelorm <gerald.elorm.gbagbe@gmail.com>
2024-09-04 15:19:56 +0000

    insert/append py

--- python/play/arr.py
+++ python/play/arr.py
@@ -1,5 +1,27 @@
 import array as array 
 
 arr = array.array('i', [1, 2, 3])
+print("The new created array is: ", end=" ")
 for i in range(0, 3):
-    print(arr[i])
+    print(arr[i], end=" ")
+
+print()
+
+# arr2 = array.array('d', [2.5, 3.2, 3.3])
+# print("The new created array is: ", end=" ")
+# for i in range(0, 3):
+#     print(arr2[i], end=" ")
+
+#inserting array using insert() method
+print("After insering into the array using the insert() method: ", end='')
+arr.insert(0, 4)
+for item in arr:
+    print(item, end=' ')
+print()
+
+print("After updating the array using the append() method: ", end=' ')
+arr.append(5)
+for item in arr:
+    print(item, end=' ')
+
+
